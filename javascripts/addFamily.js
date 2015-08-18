d567da56f287015cef2fe384739d2549aa52d010
Rick Aites <rick8s@hotmail.com>
2015-08-18 15:29:39 -0500

    added form and some other stuff

--- javascripts/addFamily.js
+++ javascripts/addFamily.js
@@ -1,1 +1,36 @@
-addFamily
+// update database
+
+define(["jquery", "q"], function($, Q) {
+    return function() {
+    var deferred = Q.defer();
+
+    $.ajax({
+      url: "https://nss-rick-family.firebaseio.com/"
+    })
+    .done(function(data) {
+      deferred.resolve(data);
+    })
+    .fail(function(xhr, status, error){
+      deferred.reject(error);
+    });
+      return deferred.promise;
+  };
+});
+
+// Submit new family member
+
+define(["jquery"], function($) {
+  $(document).on('click', '#submitInfo', function(e) {
+      e.preventDefault(); 
+      addFamily()
+      .then(function() {
+        $("#addFamily")[0].reset();
+      })
+      .fail(function(xhr, status, error) {
+        deferred.reject(error);
+      });
+    }); 
+});
+
+
+
--- javascripts/main.js
+++ javascripts/main.js
@@ -5,6 +5,7 @@ requirejs.config({
     'hbs': '../bower_components/require-handlebars-plugin/hbs',
     'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
     'firebase': '../bower_components/firebase/firebase', 
+    'q': '../bower_components/q/q'
   },
 
   shim: {
@@ -17,30 +18,31 @@ requirejs.config({
 
 requirejs(
   ["jquery", "firebase", "hbs", "bootstrap", "addFamily", "deleteFamily"], 
-  function($, lodash,  _firebase, Handlebars, bootstrap, add, delete) {
+  function($, _firebase, Handlebars, bootstrap, addFamily, deleteFamily) {
 
 
 
       // Create a referance to your Firebase database
-    var myFirebaseRef = new Firebase("https://sizzling-inferno-3854.firebaseio.com/");
+    var myFirebaseRef = new Firebase("https://nss-rick-family.firebaseio.com/");
 
       // Listen for when any changes occur to the "songs" key
-    myFirebaseRef.child("songs").on("value", function(snapshot) {
+    myFirebaseRef.child("family").on("value", function(snapshot) {
       // console.log(snapshot.val()); 
 
 
 
 
         //Store the entire songs key in a local variable
-      var songs = snapshot.val();
-      var allSongsArray = [];
+      var family = snapshot.val();
+      var allFamilyArray = [];
 
-      for (var key in songs) {
-        allSongsArray[allSongsArray.length] = songs[key];
+      for (var key in family) {
+        allFamilyArray[allFamilyArray.length] = family[key];
       }
       // console.log(allSongsArray);
 
       
+    });
   });
 
 
